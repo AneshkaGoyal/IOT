e3eb01a7fa21c70b95bccd12482fda8edbde294a
Aneshka Goyal <aneshkagoyal@gmail.com>
2019-02-08 22:39:11 +0530

    Iot P2

--- app/src/main/java/com/example/aneshkagoyal/iot/MainActivity.java
+++ app/src/main/java/com/example/aneshkagoyal/iot/MainActivity.java
@@ -48,87 +48,14 @@ public class MainActivity extends AppCompatActivity {
         t2 = (TextView) findViewById(R.id.textView3);
         b1 = (Button) findViewById(R.id.button);
         t2.setText("");
+        startService(new Intent(this, YourService.class));
 
-                try {
-                    new FetchThingspeakTask().execute();
-                } catch (Exception e) {
-                    Log.e("ERROR", e.getMessage(), e);
-                }
+
+
 
     }
 
-    class FetchThingspeakTask extends AsyncTask<Void, Void, String> {
-        protected void onPreExecute() {
-            t2.setText("Fetching Data from Server.Please Wait...");
-        }
 
-        protected String doInBackground(Void... urls) {
-            try {
-                URL url = new URL("https://api.thingspeak.com/channels/687247/feeds.json?api_key=6NSETZZWR6HBO6J8&results=2");
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                try {
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                    StringBuilder stringBuilder = new StringBuilder();
-                    String line;
-                    while ((line = bufferedReader.readLine()) != null) {
-                        stringBuilder.append(line).append("\n");
-                    }
-                    bufferedReader.close();
-                    return stringBuilder.toString();
-                } finally {
-                    urlConnection.disconnect();
-                }
-            } catch (Exception e) {
-                Log.e("ERROR", e.getMessage(), e);
-
-
-                return null;
-            }
-        }
-
-        protected void onPostExecute(String response) {
-            if (response == null) {
-                Toast.makeText(MainActivity.this, "There was an error", Toast.LENGTH_SHORT).show();
-                return;
-            }
-            try {
-                //  t1.setText(response);
-                JSONObject channel = (JSONObject) new JSONObject(response);
-                // t1.setText(""+channel);
-                JSONArray feed = channel.getJSONArray("feeds");
-                JSONObject first = feed.getJSONObject(0);
-                // t1.setText(""+first);
-                String v1 = first.getString("field2");
-                if (!v1.equals("40.00")) {
-                    manager = (AlarmManager) getSystemService(ALARM_SERVICE);
-
-
-                    Intent intent = new Intent(MainActivity.this, BroadCast.class);
-                    PendingIntent broadcast = PendingIntent.getBroadcast(MainActivity.this, 100, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-                    long time = System.currentTimeMillis();
-                    //manager.set(AlarmManager.RTC_WAKEUP,cc.getTimeInMillis(),broadcast);
-                    notificationCall(time + 5 * 1000, broadcast);
-
-
-                    //setAlarm();
-
-
-                }
-
-                //    if(v1>=90)
-                t1.setText("" + v1);
-
-                //  else
-                //    t1.setText("NO VALUES");
-                try {
-                    new FetchThingspeakTask().execute();
-                } catch (Exception e) {
-                    Log.e("ERROR", e.getMessage(), e);
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
     }
 
     /*EditText in;
@@ -166,13 +93,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }*/
-    @SuppressLint("NewApi")
-    private void notificationCall(long time, PendingIntent pIntent) {
-        if (android.os.Build.VERSION.SDK_INT >= 19) {
-            manager.setExact(AlarmManager.RTC_WAKEUP, time, pIntent);
-        } else {
-            manager.set(AlarmManager.RTC_WAKEUP, time, pIntent);
-        }
-    }
-}
+
+
 
